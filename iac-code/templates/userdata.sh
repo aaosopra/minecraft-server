2c5a13f8bb17c44df1a028496f52dc85109e3a63
Askil <aao@example.com>
2024-08-22 10:20:14 +0200

    updated to use docker compose v2

--- iac-code/templates/userdata.sh
+++ iac-code/templates/userdata.sh
@@ -6,4 +6,5 @@ sudo apt install ansible git -y
 wget ${playbook_url} -O /tmp/server_config.yaml
 ansible-galaxy collection install ansible.posix
 ansible-galaxy collection install community.general
+ansible-galaxy collection install community.docker
 ansible-playbook /tmp/server_config.yaml --connection=local
